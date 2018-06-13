f873ee0827f71cfaa08539f5fabdeb68d41e99d7
Bohao Wang <bj050323@gmail.com>
2018-06-13 23:09:44 +0800

    createStore

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 function createStore(reducer) {
-  
-}
+  var state = null;
+  var listeners = [];
 
+  function getState() {
+    return state || 0;
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    listeners.forEach(v => v());
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
+}
