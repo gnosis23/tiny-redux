a5104de02edf8c31ce1775604282d45573c11065
Bohao Wang <bj050323@gmail.com>
2018-06-15 23:31:56 +0800

    naive applyMiddleware

--- src/applyMiddleware.js
+++ src/applyMiddleware.js
@@ -1,8 +1,10 @@
 // return (store) => new store
-export default function applyMiddleware(middlewares) {
+export default function applyMiddleware(...middlewares) {
   return (store) => {
+    // soft copy
+    middlewares = middlewares.slice();
+    middlewares.reverse();
     const mids = middlewares.map(m => m(store));
-    mids.reverse();
     store.dispatch = mids.reduce(
       (dis, mid) => mid(dis),
       store.dispatch
