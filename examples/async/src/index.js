fe8bed8e8b25f36c3f0bc7ba026b0295861ee733
Bohao Wang <bj050323@gmail.com>
2018-06-16 22:20:41 +0800

    example async

--- examples/async/src/index.js
+++ examples/async/src/index.js
@@ -1,20 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from './redux'
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from './redux'
 import Counter from './components/Counter'
 import counter from './reducers'
 
-const store = createStore(counter)
-const rootEl = document.getElementById('root')
+const store = createStore(counter, applyMiddleware(thunk));
+const rootEl = document.getElementById('root');
+
+function incrementAsync() {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch({type: 'INCREMENT'})
+    }, 1000);
+  }
+}
 
 const render = () => ReactDOM.render(
   <Counter
     value={store.getState()}
     onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
     onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    onIncrementAsync={() => store.dispatch(incrementAsync())}
   />,
   rootEl
-)
+);
 
-render()
-store.subscribe(render)
+render();
+store.subscribe(render);
--- examples/async/src/redux.js
+++ examples/async/src/redux.js
@@ -2,7 +2,7 @@ function createStore(reducer, enhancer) {
   var state = null;
   var listeners = [];
   if (enhancer) {
-    return enhancer(createStore(reducer));
+    return enhancer(createStore)(reducer);
   }
 
   function getState() {
@@ -27,18 +27,61 @@ function createStore(reducer, enhancer) {
   };
 }
 
+var _extends = Object.assign || function (target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i];
+
+    for (var key in source) {
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        target[key] = source[key];
+      }
+    }
+  }
+
+  return target;
+};
+
 // return (store) => new store
-function applyMiddleware(middlewares) {
-  return function (store) {
-    var mids = middlewares.map(function (m) {
-      return m(store);
-    });
-    mids.reverse();
-    var dispatch = mids.reduce(function (dis, mid) {
-      return mid(dis);
-    }, store.dispatch);
-    store.dispatch = dispatch;
-    return store;
+function applyMiddleware() {
+  for (var _len = arguments.length, middlewares = Array(_len), _key = 0; _key < _len; _key++) {
+    middlewares[_key] = arguments[_key];
+  }
+
+  return function (createStore) {
+    return function () {
+      for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+        args[_key2] = arguments[_key2];
+      }
+
+      var store = createStore.apply(undefined, args);
+      var _dispatch = function dispatch() {
+        throw new Error("ispatching while constructing your middleware is not allowed." + "Other middleware would not be applied to this dispatch.");
+      };
+
+      var middlewareAPI = {
+        getState: store.getState,
+        // the dispatch passed in here must be proxied
+        // this dispatch will be reassigned
+        dispatch: function dispatch() {
+          return _dispatch.apply(undefined, arguments);
+        }
+      };
+
+      // soft copy
+      middlewares = middlewares.slice();
+      middlewares.reverse();
+
+      var mids = middlewares.map(function (m) {
+        return m(middlewareAPI);
+      });
+      _dispatch = mids.reduce(function (dis, mid) {
+        return mid(dis);
+      }, store.dispatch);
+
+      return _extends({}, store, {
+        dispatch: _dispatch
+      });
+    };
   };
 }
 
