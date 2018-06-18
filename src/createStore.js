a3671bccf2f5717ae29ba4f4c799c400a1296a55
Bohao Wang <bj050323@gmail.com>
2018-06-18 15:32:31 +0800

    createStore tests 2

--- src/createStore.js
+++ src/createStore.js
@@ -1,4 +1,5 @@
 import ActionTypes from './utils/actionTypes';
+import isPlainObject from './utils/isPlainObject'
 
 export default function createStore(reducer, preloadedState, enhancer) {
   if (typeof preloadedState === 'function' && typeof enhancer === 'undefined') {
@@ -20,21 +21,73 @@ export default function createStore(reducer, preloadedState, enhancer) {
 
   let state = preloadedState;
   let listeners = [];
+  let isDispatching = false;
 
   function getState() {
+    if (isDispatching) {
+      throw new Error(
+        'You may not call store.getState() while the reducer is executing. ' +
+        'The reducer has already received the state as an argument. ' +
+        'Pass it down from the top reducer instead of reading it from the store.'
+      );
+    }
     return state;
   }
 
   function subscribe(listener) {
-    listeners.push(listener);
-    return () => {
-      listeners = listeners.filter(func => func !== listener);
+    if (typeof listener !== 'function') {
+      throw new Error('Expected the listener to be a function.')
     }
+
+    if (isDispatching) {
+      throw new Error(
+        'You may not call store.subscribe() while the reducer is executing. ' +
+        'If you would like to be notified after the store has been updated, subscribe from a ' +
+        'component and invoke store.getState() in the callback to access the latest state. ' +
+        'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.'
+      );
+    }
+
+    function unlistener() {
+      if (isDispatching) {
+        throw new Error(
+          'You may not unsubscribe from a store listener while the reducer is executing. ' +
+          'See https://redux.js.org/api-reference/store#subscribe(listener) for more details.'
+        )
+      }
+
+      listeners = listeners.filter(v => v.unlistener !== unlistener);
+    }
+    listeners.push({
+      listener: listener.bind(undefined),
+      unlistener
+    });
+    return unlistener;
   }
 
   function dispatch(action) {
-    state = reducer(state, action);
-    listeners.forEach(v => v());
+    if (!isPlainObject(action)) {
+      throw new Error('action is not a plain object');
+    }
+
+    if (typeof action.type === 'undefined') {
+      throw new Error(
+        'Actions may not have an undefined "type" property. ' +
+        'Have you misspelled a constant?'
+      )
+    }
+
+    if (isDispatching) {
+      throw new Error('Reducers may not dispatch actions.')
+    }
+
+    try {
+      isDispatching = true;
+      state = reducer(state, action);
+    } finally {
+      isDispatching = false;
+    }
+    listeners.forEach(v => v.listener());
   }
 
   // When a store is created, an "INIT" action is dispatched so that every
--- test/createStore.spec.js
+++ test/createStore.spec.js
@@ -1,6 +1,13 @@
 import { combineReducers, createStore } from '../';
 import * as reducers from './helpers/reducers';
-import { addTodo, unknownAction } from './helpers/actionCreators';
+import {
+  addTodo,
+  dispatchInMiddle,
+  getStateInMiddle,
+  subscribeInMiddle,
+  unsubscribeInMiddle,
+  unknownAction
+} from './helpers/actionCreators';
 // import * as Rx from 'rxjs'
 // import $$observable from 'symbol-observable'
 
@@ -180,6 +187,229 @@ describe('createStore', () => {
     store.dispatch(unknownAction());
     expect(listener.mock.calls.length).toBe(1);
   });
+
+  it('supports removing a subscription within a subscription', () => {
+    const store = createStore(reducers.todos);
+    const listenerA = jest.fn();
+    const listenerB = jest.fn();
+    const listenerC = jest.fn();
+
+    store.subscribe(listenerA);
+    const unSubB = store.subscribe(() => {
+      listenerB();
+      unSubB();
+    });
+    store.subscribe(listenerC);
+
+    store.dispatch(unknownAction());
+    store.dispatch(unknownAction());
+
+    expect(listenerA.mock.calls.length).toBe(2);
+    expect(listenerB.mock.calls.length).toBe(1);
+    expect(listenerC.mock.calls.length).toBe(2);
+  });
+
+  it(
+    'notifies all subscribers about current dispatch regardless if any of them gets unsubscribed in the process',
+    () => {
+      const store = createStore(reducers.todos);
+
+      const unsubscribeHandles = [];
+      const doUnsubscribeAll = () =>
+        unsubscribeHandles.forEach(unsubscribe => unsubscribe());
+
+      const listener1 = jest.fn();
+      const listener2 = jest.fn();
+      const listener3 = jest.fn();
+
+      unsubscribeHandles.push(store.subscribe(() => listener1()));
+      unsubscribeHandles.push(
+        store.subscribe(() => {
+          listener2();
+          doUnsubscribeAll();
+        })
+      );
+      unsubscribeHandles.push(store.subscribe(() => listener3()));
+
+      store.dispatch(unknownAction());
+      expect(listener1.mock.calls.length).toBe(1);
+      expect(listener2.mock.calls.length).toBe(1);
+      expect(listener3.mock.calls.length).toBe(1);
+
+      store.dispatch(unknownAction());
+      expect(listener1.mock.calls.length).toBe(1);
+      expect(listener2.mock.calls.length).toBe(1);
+      expect(listener3.mock.calls.length).toBe(1);
+    });
+
+  it('notifies only subscribers active at the moment of current dispatch',
+    () => {
+      const store = createStore(reducers.todos);
+
+      const listener1 = jest.fn();
+      const listener2 = jest.fn();
+      const listener3 = jest.fn();
+
+      let listener3Added = false;
+      const maybeAddThirdListener = () => {
+        if (!listener3Added) {
+          listener3Added = true;
+          store.subscribe(() => listener3());
+        }
+      };
+
+      store.subscribe(() => listener1());
+      store.subscribe(() => {
+        listener2();
+        maybeAddThirdListener();
+      });
+
+      store.dispatch(unknownAction());
+      expect(listener1.mock.calls.length).toBe(1);
+      expect(listener2.mock.calls.length).toBe(1);
+      expect(listener3.mock.calls.length).toBe(0);
+
+      store.dispatch(unknownAction());
+      expect(listener1.mock.calls.length).toBe(2);
+      expect(listener2.mock.calls.length).toBe(2);
+      expect(listener3.mock.calls.length).toBe(1);
+    });
+
+  it('uses the last snapshot of subscribers during nested dispatch', () => {
+    const store = createStore(reducers.todos);
+
+    const listener1 = jest.fn();
+    const listener2 = jest.fn();
+    const listener3 = jest.fn();
+    const listener4 = jest.fn();
+
+    let unsubscribe4;
+    const unsubscribe1 = store.subscribe(() => {
+      listener1();
+      expect(listener1.mock.calls.length).toBe(1);
+      expect(listener2.mock.calls.length).toBe(0);
+      expect(listener3.mock.calls.length).toBe(0);
+      expect(listener4.mock.calls.length).toBe(0);
+
+      unsubscribe1();
+      unsubscribe4 = store.subscribe(listener4);
+      store.dispatch(unknownAction());
+
+      expect(listener1.mock.calls.length).toBe(1);
+      expect(listener2.mock.calls.length).toBe(1);
+      expect(listener3.mock.calls.length).toBe(1);
+      expect(listener4.mock.calls.length).toBe(1);
+    });
+    store.subscribe(listener2);
+    store.subscribe(listener3);
+
+    store.dispatch(unknownAction());
+    expect(listener1.mock.calls.length).toBe(1);
+    expect(listener2.mock.calls.length).toBe(2);
+    expect(listener3.mock.calls.length).toBe(2);
+    expect(listener4.mock.calls.length).toBe(1);
+
+    unsubscribe4();
+    store.dispatch(unknownAction());
+    expect(listener1.mock.calls.length).toBe(1);
+    expect(listener2.mock.calls.length).toBe(3);
+    expect(listener3.mock.calls.length).toBe(3);
+    expect(listener4.mock.calls.length).toBe(1);
+  });
+
+  it('provides an up-to-date state when a subscriber is notified', done => {
+    const store = createStore(reducers.todos);
+    store.subscribe(() => {
+      expect(store.getState()).toEqual([
+        {
+          id: 1,
+          text: 'Hello'
+        }
+      ]);
+      done();
+    });
+    store.dispatch(addTodo('Hello'));
+  });
+
+  it('does not leak private listeners array', done => {
+    const store = createStore(reducers.todos);
+    store.subscribe(function () {
+      expect(this).toBe(undefined);
+      done();
+    });
+    store.dispatch(addTodo('Hello'));
+  });
+
+  it('only accepts plain object actions', () => {
+    const store = createStore(reducers.todos);
+    expect(() => store.dispatch(unknownAction())).not.toThrow();
+
+    function AwesomeMap () {}
+    [null, undefined, 42, 'hey', new AwesomeMap()].forEach(nonObject =>
+      expect(() => store.dispatch(nonObject)).toThrow(/plain/)
+    );
+  });
+
+  it('handles nested dispatches gracefully', () => {
+    function foo (state = 0, action) {
+      return action.type === 'foo' ? 1 : state;
+    }
+
+    function bar (state = 0, action) {
+      return action.type === 'bar' ? 2 : state;
+    }
+
+    const store = createStore(combineReducers({foo, bar}));
+
+    store.subscribe(function kindaComponentDidUpdate () {
+      const state = store.getState();
+      if (state.bar === 0) {
+        store.dispatch({type: 'bar'});
+      }
+    });
+
+    store.dispatch({type: 'foo'});
+    expect(store.getState()).toEqual({
+      foo: 1,
+      bar: 2
+    });
+  });
+
+  it('does not allow dispatch() from within a reducer', () => {
+    const store = createStore(reducers.dispatchInTheMiddleOfReducer);
+
+    expect(() =>
+      store.dispatch(
+        dispatchInMiddle(store.dispatch.bind(store, unknownAction()))
+      )
+    ).toThrow(/may not dispatch/);
+  });
+
+  it('does not allow getState() from within a reducer', () => {
+    const store = createStore(reducers.getStateInTheMiddleOfReducer);
+
+    expect(() =>
+      store.dispatch(getStateInMiddle(store.getState.bind(store)))
+    ).toThrow(/You may not call store.getState()/);
+  });
+
+  it('does not allow subscribe() from within a reducer', () => {
+    const store = createStore(reducers.subscribeInTheMiddleOfReducer);
+
+    expect(() =>
+      store.dispatch(subscribeInMiddle(store.subscribe.bind(store, () => {})))
+    ).toThrow(/You may not call store.subscribe()/);
+  });
+
+  it('does not allow unsubscribe from subscribe() from within a reducer',
+    () => {
+      const store = createStore(reducers.unsubscribeInTheMiddleOfReducer);
+      const unsubscribe = store.subscribe(() => {});
+
+      expect(() =>
+        store.dispatch(unsubscribeInMiddle(unsubscribe.bind(store)))
+      ).toThrow(/You may not unsubscribe from a store/);
+    });
 
 
 });
